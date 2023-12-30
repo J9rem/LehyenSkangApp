31d8c96e38f2d5790a1b3db9a698747511fe4961
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-12-30 22:37:56 +0100

    fix(Pico): make it compatible with new ParseDown

--- src/php/Pico.php
+++ src/php/Pico.php
@@ -7,9 +7,34 @@
 
 namespace LehyenSkang;
 
+use Erusev\Parsedown\Configurables\Breaks;
+use Erusev\Parsedown\Parsedown;
+use Erusev\Parsedown\State;
+use Erusev\ParsedownExtra\ParsedownExtra;
 use Pico as PicoRaw;
 
 class Pico extends PicoRaw
 {
+    public function getParsedown(): Parsedown
+    {
+        if ($this->parsedown === null) {
+            $state = new State([
+                new Breaks((bool) $this->config['content_config']['breaks'])
+            ]);
+            if ($this->config['content_config']['extra']){
+                $state = new ParsedownExtra($state);
+            }
+            $this->parsedown = new Parsedown($state);
 
+            $this->triggerEvent('onParsedownRegistered', [ &$this->parsedown ]);
+        }
+
+        return $this->parsedown;
+    }
+
+    public function parseFileContent(string $markdown, bool $singleLine = false): string
+    {
+        $markdownParser = $this->getParsedown();
+        return @$markdownParser->toHtml($markdown);
+    }
 }
